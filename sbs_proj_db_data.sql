9c1a82456400ad6d0436772a1b60b76c0818c833
rantinum <syjcode5724@gmail.com>
2023-07-17 20:52:39 +0900

    히트컬럼 추가, sql 업데이트

--- sbs_proj_db_data.sql
+++ sbs_proj_db_data.sql
@@ -21,77 +21,88 @@ USE `sbs_proj`;
 DROP TABLE IF EXISTS `article`;
 
 CREATE TABLE `article` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `regDate` DATETIME NOT NULL,
-  `updateDate` DATETIME NOT NULL,
-  `title` CHAR(100) NOT NULL,
-  `body` CHAR(100) NOT NULL,
-  `memberId` INT(1) UNSIGNED NOT NULL,
-  `boardId` INT(1) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `regDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `title` char(100) NOT NULL,
+  `body` char(100) NOT NULL,
+  `memberId` int(1) unsigned NOT NULL,
+  `boardId` int(1) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `boardId` (`boardId`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `article` */
+
+insert  into `article`(`id`,`regDate`,`updateDate`,`title`,`body`,`memberId`,`boardId`) values 
+(1,'2023-07-14 12:08:08','2023-07-14 12:08:08','제목1','내용1',1,1),
+(2,'2023-07-14 12:08:30','2023-07-14 12:08:30','제목2','내용2',2,1),
+(3,'2023-07-14 12:08:30','2023-07-14 12:08:30','제목3','내용3',1,2);
 
 /*Table structure for table `articleReply` */
 
 DROP TABLE IF EXISTS `articleReply`;
 
 CREATE TABLE `articleReply` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `regDate` DATETIME NOT NULL,
-  `updateDate` DATETIME NOT NULL,
-  `body` CHAR(100) NOT NULL,
-  `memberId` INT(1) UNSIGNED NOT NULL,
-  `articleId` INT(1) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `regDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `body` char(100) NOT NULL,
+  `memberId` int(1) unsigned NOT NULL,
+  `articleId` int(1) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `articleId` (`articleId`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `articleReply` */
+
+insert  into `articleReply`(`id`,`regDate`,`updateDate`,`body`,`memberId`,`articleId`) values 
+(1,'2023-07-14 12:09:44','2023-07-14 12:09:44','댓글1',1,2),
+(2,'2023-07-14 12:09:44','2023-07-14 12:09:44','댓글2',2,1);
 
 /*Table structure for table `board` */
 
 DROP TABLE IF EXISTS `board`;
 
 CREATE TABLE `board` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `regDate` DATETIME NOT NULL,
-  `updateDate` DATETIME NOT NULL,
-  `code` CHAR(100) NOT NULL,
-  `name` CHAR(100) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `regDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `code` char(100) NOT NULL,
+  `name` char(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `code` (`code`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-SELECT * FROM article;
-INSERT INTO article
-updateDAte = NOW(),
-title = '제목1'
-`body` = '내용1'
-memberId = 1
-boardId = 1;
 /*Data for the table `board` */
+
+insert  into `board`(`id`,`regDate`,`updateDate`,`code`,`name`) values 
+(1,'2023-07-14 12:12:39','2023-07-14 12:12:39','notice','공지'),
+(2,'2023-07-14 12:12:39','2023-07-14 12:12:39','free','자유');
 
 /*Table structure for table `member` */
 
 DROP TABLE IF EXISTS `member`;
 
 CREATE TABLE `member` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `regDate` DATETIME NOT NULL,
-  `updateDate` DATETIME NOT NULL,
-  `loginId` CHAR(100) NOT NULL,
-  `loginPw` CHAR(100) NOT NULL,
-  `name` CHAR(100) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `regDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `loginId` char(100) NOT NULL,
+  `loginPw` char(100) NOT NULL,
+  `name` char(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `loginId` (`loginId`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `member` */
 
+insert  into `member`(`id`,`regDate`,`updateDate`,`loginId`,`loginPw`,`name`) values 
+(1,'2023-07-14 12:11:13','2023-07-14 12:11:13','admin','admin','관리자'),
+(2,'2023-07-14 12:11:40','2023-07-14 12:11:40','user1','user1','유저1'),
+(3,'2023-07-14 12:11:40','2023-07-14 12:11:40','user2','user2','유저2');
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS *`article``articleReply``sbs_proj`/;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
